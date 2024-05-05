43e73c016d30d829b717780caa2783e7cc7c0920
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-05-05 11:10:03 +0700

    add light and dark theme

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              // logo
+
+              // app name
+
+              // email textfield
+
+              // password textfield
+
+              // forgot password
+
+              // sign in button
+
+              // dont have account? register here
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
