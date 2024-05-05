eb11c520ba36248dd3c9e5cf5983dcbaf29b9190
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-05-05 13:57:20 +0700

    setup firebase core and firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minimal_social_app/auth/login_or_register.dart';
+import 'package:minimal_social_app/firebase_options.dart';
 import 'package:minimal_social_app/theme/dark_mode.dart';
 import 'package:minimal_social_app/theme/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
